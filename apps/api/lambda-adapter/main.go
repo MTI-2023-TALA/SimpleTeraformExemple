5b3f75308f69930ebdf69d10a1dabd325a709d73
Alois Heloir <alois.heloir@live.fr>
2022-11-04 08:24:35 +0100

    Add adapter for lambda

---
+++ apps/api/lambda-adapter/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"simple-teraform-exemple/apps/api/controller"
+	"simple-teraform-exemple/apps/api/initializers"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
+	"github.com/gofiber/fiber/v2"
+)
+
+var fiberLambda *fiberadapter.FiberLambda
+
+func init() {
+	log.Printf("Fiber cold start")
+	initializers.ConnectToDB()
+
+
+	var app *fiber.App
+	app = fiber.New()
+
+	app.Post("/todo", controller.CreateTodo)
+	app.Put("/todo/:id", controller.UpdateTodo)
+	app.Get("/todo", controller.GetAllTodo)
+	app.Get("/todo/:id", controller.GetOneTodo)
+	app.Delete("/todo/:id", controller.DeleteTodo)
+
+	fiberLambda = fiberadapter.New(app)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return fiberLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(Handler)
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aws/aws-lambda-go v1.34.1 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.13.3 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
